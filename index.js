261d7d7a6ef13f3c5be3b89d52be27bcecf15f45
Chibueze Osuoji <chibz2006@yahoo.com>
2019-01-24 11:30:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ function logWhisper(string)
 {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string)
+{
+  if(string === lowercase)
+  {
+    return "I can't hear you!"
+  } 
+
+  else if (string === uppercase) 
+  {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.")
+  {
+    return ("I love you,too")
+  }
+}
